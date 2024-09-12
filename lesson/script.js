87bc544dc91e503abaa9787192608fbaeed2a78b
Antipov228 <antipov8481@gmail.com>
2024-09-12 14:48:32 +1000

    Add files via upload

---
+++ lesson/script.js
@@ -0,0 +1,81 @@
+
+
+//const cities = ['MAdrid','moskow','Vladivostok','muhosransk'];
+
+//const cities2 = ['Artem',...cities, 'Ussurisk'];
+
+//console.log(cities);
+//console.log(...cities);
+//console.log(cities2);
+
+//const cities = ['Moskow','Vladivostok','Khabarovsk'];
+
+//const [MSK,VLK,KHV] = cities;
+
+//console.log(KHV,MSK)
+
+
+
+//const cities = ['Moskow','Vladivostok','Khabarovsk','Nahodka','Artem'];
+
+//const [MSK,VLK,KHV, ...tails,] = cities;
+
+//console.log(VLK,...tails)
+//console.log(VLK,tails)
+
+
+//const age = prompt("введите свой текст:");
+//document.write("Вам" + age + "лет");
+//function openwindow(){
+  //  window.open("https://www.vvsu.ru");
+//}
+//function openwindow(){
+  //  const popup = window.open("https://www.vvsu.ru");
+//setTimeout(() => popup.close(),5000);
+//}
+// 1
+// function getname(){
+//   let name = prompt("введите ваше имя...");
+//   console.log(name)
+// }
+// getname()
+
+
+// 4
+// const mas = [
+//   {
+//     id:1,
+//     name: "sergey",
+//     job: "Devops",
+//     salary: 10000,
+//   },
+//   {
+//     id:2,
+//     name: "Matvey",
+//     job: "fullstack",
+//     salary:20000,
+//   },
+//   {
+//     id:1,
+//     name: "stepan",
+//     job: "CEO",
+//     salary:100000,
+//   },
+//   {
+//     id:1,
+//     name: "stepan",
+//     job: "CEO",
+//     salary:100000,
+//   },
+// ]
+// function getSalary(){
+//   allSalary = 0;
+//   for (let i = 0; i < mas.length; i++){
+//     allSalary += mas[i].salary
+//   }
+//   console.log(allSalary)
+// }
+// getSalary()
+
+
+
